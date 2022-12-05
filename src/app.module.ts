488d3a9282bf581500ab32deaafdd9bdb474d7ea
loiskouninef <lkouninef@gmail.com>
2022-12-05 10:54:43 +0100

    scaffolding mailer, auths, update package & nest-cli

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,11 @@ import { UsersModule } from './users/users.module';
 import { BooksModule } from './books/books.module';
 import { CommentsModule } from './comments/comments.module';
 import { FavoritesModule } from './favorites/favorites.module';
+import { AuthenticationModule } from './authentication/authentication.module';
+import { MailerModule } from './mailer/mailer.module';
 
 @Module({
-  imports: [UsersModule, BooksModule, CommentsModule, FavoritesModule],
+  imports: [UsersModule, BooksModule, CommentsModule, FavoritesModule, AuthenticationModule, MailerModule],
   controllers: [AppController],
   providers: [AppService],
 })
