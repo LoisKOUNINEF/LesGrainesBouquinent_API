a92a59ec7d49979d93db42d475e122bf37a011e2
loiskouninef <lkouninef@gmail.com>
2022-12-05 10:24:24 +0100

    scaffold resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { BooksModule } from './books/books.module';
+import { CommentsModule } from './comments/comments.module';
+import { FavoritesModule } from './favorites/favorites.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, BooksModule, CommentsModule, FavoritesModule],
   controllers: [AppController],
   providers: [AppService],
 })
